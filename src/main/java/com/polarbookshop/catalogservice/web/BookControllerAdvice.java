0781206ea95714b931bdbd0fcfe892dee348558d
sth <sth@yihuosoft.com>
2023-04-21 14:18:35 +0800

    3.3.3 Data validation and error handling

---
+++ src/main/java/com/polarbookshop/catalogservice/web/BookControllerAdvice.java
@@ -0,0 +1,63 @@
+package com.polarbookshop.catalogservice.web;
+
+import com.polarbookshop.catalogservice.domain.BookAlreadyExistsException;
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class BookControllerAdvice {
+
+    /**
+     * an HTTP response with status 422 (unprocessable entity)
+     * when we try to create a book that already exists in the catalog.
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(BookAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    String bookAlreadyExistsHandler(BookAlreadyExistsException ex) {
+        return ex.getMessage();
+    }
+
+    /**
+     * a response with status 404 (not found)
+     * when we try to read a book that doesn’t exist.
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(BookNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String bookNotFoundHandler(BookNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    /**
+     * a response with status 400 (bad request)
+     * when one or more fields in a Book object are invalid.
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+}
