eea209c9462e21466c79b9f37c93b5fc156b6925
ivypas <47619898+Ivypas@users.noreply.github.com>
2023-04-24 11:58:08 +0800

    5.2.3 Enabling and configuring JDBC auditing
    To update the autotests accordingly, when necessary. You can also extend the tests in BookJsonTests to verify the serialization and deserialization of the new fields.

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,12 +1,15 @@
 package com.polarbookshop.catalogservice.domain;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import java.time.Instant;
 
 public record Book(
         @Id
@@ -21,10 +24,14 @@ public record Book(
         @NotNull(message = "The book price must be defined.")
         @Positive(message = "The book price must be greater than zero.")
         Double price,
+        @CreatedDate
+        Instant createdDate,
+        @LastModifiedDate
+        Instant lastModifiedDate,
         @Version
         int version
 ) {
     public static Book of(String isbn, String title, String author, Double price) {
-        return new Book(null, isbn, title, author, price, 0);
+        return new Book(null, isbn, title, author, price, null, null, 0);
     }
 }
